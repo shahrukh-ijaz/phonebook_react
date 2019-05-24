2a423923c40b34f0711cfbe1160245f0a902c14e
Shahrukh.Ijaz <shahrukh@A002-00097.arbisoft.com>
2019-05-24 15:03:52 +0500

    assertions added in tests

--- cypress/integration/Test_Phonebook/test_login.js
+++ cypress/integration/Test_Phonebook/test_login.js
@@ -19,7 +19,7 @@ describe('Login Test', function(){
                             cy.get('a#logout_link').click().then(response=>{
                                 expect(true).to.be.true
                             });
-                            expect(true).to.be.true
+                            expect(false).to.be.false
                         });
                         expect(true).to.be.true
                     });
