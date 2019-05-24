337d59cadcc564cf823fbe2fdce49e9c56dc701a
Shahrukh.Ijaz <shahrukh@A002-00097.arbisoft.com>
2019-05-24 15:02:15 +0500

    assertions added in tests

--- cypress/integration/Test_Phonebook/test_register.js
+++ cypress/integration/Test_Phonebook/test_register.js
@@ -1,13 +1,17 @@
-describe('Login Test', function(){
-    it('Enter Username, password and do login', function () {
+describe('Register Test', function(){
+    it('Enter Details for Signup', function () {
         // expect(true).to.equal(true);
-        cy.visit('http://localhost:3006/register');
+        cy.visit('http://localhost:3006/register')
 
         cy.get('input#email').type('ali_1@gmail.com');
         cy.get('input#password').type('ali123');
         cy.get('input#username').type('ali123');
         cy.get('input#first_name').type('ali');
         cy.get('input#last_name').type('raza');
-        cy.get('button[type="submit"]').click();
+        cy.get('button[type="submit"]').click().then(response=>{
+            expect(true).to.be.true
+        });
+            expect(false).to.be.false
+
     })
-})
+});
--- cypress/integration/Test_Phonebook/test_stub_login.js
+++ cypress/integration/Test_Phonebook/test_stub_login.js
@@ -1,12 +1,11 @@
 // cypress/integration/test_stub_login.js
 
-describe("Testing the widget", function() {
-    it("Widget does something with AJAX / XHR", function() {
+describe("API Stub Testing", function() {
+    it("Request Fixture Testing", function() {
         cy
             .request('POST', 'http://127.0.0.1:8001/api/login', { username: 'shahrukh-ijaz', password: 'shahrukh31' })
             .then((response) => {
-                // response.body is automatically serialized into JSON
-               expect(response.body).to.have.property('token', '8a404e47938e966ff6d4b219db236c1e5c02a4c7') // true
-            })
+               expect(response.body).to.have.property('token') // true
+            });
     });
 });
